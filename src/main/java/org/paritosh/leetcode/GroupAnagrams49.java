02a07cc3f80a3622e12a5967da5ad7e100e3f458
Paritosh Kumar <ashokkyadav200@gmail.com>
2025-10-08 00:41:37 +0530

    49. Group Anagrams

---
+++ src/main/java/org/paritosh/leetcode/GroupAnagrams49.java
@@ -0,0 +1,41 @@
+package org.paritosh.leetcode;
+
+import java.util.*;
+
+class GroupAnagrams49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs==null || strs.length==0)
+            return new ArrayList<>();
+
+        HashMap<String,List<String>> freqStrMap = new HashMap<>();
+        for(String str : strs){
+            String freqStr =  getStrFreq(str);
+            if(freqStrMap.containsKey(freqStr)){
+                freqStrMap.get(freqStr).add(str);
+            }else{
+                List<String> strList = new ArrayList<>();
+                strList.add(str);
+                freqStrMap.put(freqStr,strList);
+            }
+        }
+        return new ArrayList<>(freqStrMap.values());
+    }
+
+    public String getStrFreq(String str){
+
+        int[] freq = new int[26];
+        for(char c : str.toCharArray()){
+            freq[c-'a']++;
+        }
+
+        StringBuilder freqStr = new StringBuilder("");
+
+        char c='a';
+        for(int i : freq){
+            freqStr.append(c);
+            freqStr.append(i);
+            c++;
+        }
+        return freqStr.toString();
+    }
+}
